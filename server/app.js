76f358f4d1bcaf94490ce51705acc7eb1d31451b
Vasil Vasilev <89039078+SookX@users.noreply.github.com>
2025-10-19 20:31:17 +0300

    Setup React

--- server/app.js
+++ server/app.js
@@ -12,6 +12,10 @@ app.use(express.json());
 app.use(cors())
 app.use(express.static(path.join(__dirname, '/client/dist')))
 
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
 
 app.use(globalErrorHandler);
 
